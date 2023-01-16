b85afa9a6b8fdc2f5a2ab1d8767e870549e0eeb5
cartwatson <cartwatson@gmail.com>
2023-01-16 13:21:12 -0700

    demo program to spit out all args

---
+++ 1assign/Assign1.java
@@ -0,0 +1,8 @@
+public class Assign1 {
+    public static void main(String[] args) {
+        int length = args.length;
+        for (int i = 0; i < length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
